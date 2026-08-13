4275d83cf7933ecac6d74f83057230ac42a803b3
Fabian Guerrero <guerrerofabian1985@gmail.com>
2026-08-13 13:17:43 +0200

    Crear clase Cliente

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,34 @@
+public class Cliente {
+    private String nombre;
+    private String telefono;
+
+    public Cliente(String nombre, String telefono){
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Cliente{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", telefono='").append(telefono).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
