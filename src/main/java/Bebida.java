25b4d3999a9edd7531178db37b6604b30906e317
Fabian Guerrero <guerrerofabian1985@gmail.com>
2026-08-13 13:07:16 +0200

    Crear clases Bebida y Comida, hijas de Producto

---
+++ src/main/java/Bebida.java
@@ -0,0 +1,26 @@
+public class Bebida extends Producto{
+    private String tamano;
+
+    public Bebida(String nombre , double precio, String Categoria, String tamano) {
+        super(nombre, precio, Categoria);
+        this.tamano = tamano;
+    }
+
+    public String getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(String tamano) {
+        this.tamano = tamano;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Bebida{");
+        sb.append("tamano='").append(tamano).append('\'');
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append(", precio=").append(precio);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/Comida.java
@@ -0,0 +1,26 @@
+public class Comida extends Producto{
+    private boolean esCaliente;
+
+    public Comida(String nombre, double precio, String categoria, boolean esCaliente){
+        super(nombre, precio, categoria);
+        this.esCaliente = esCaliente;
+    }
+
+    public boolean isEsCaliente() {
+        return esCaliente;
+    }
+
+    public void setEsCaliente(boolean esCaliente) {
+        this.esCaliente = esCaliente;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Comida{");
+        sb.append("esCaliente=").append(esCaliente);
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append(", precio=").append(precio);
+        sb.append('}');
+        return sb.toString();
+    }
+}
