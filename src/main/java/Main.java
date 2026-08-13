4d976191cd7446048975da7743f2b0148cc11460
Fabian Guerrero <guerrerofabian1985@gmail.com>
2026-08-13 16:59:03 +0200

    Añadir funcionalidad para crear camarero

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,9 @@ public class Main {
     private static Cliente[] clientes = new Cliente[10];
     private static int numClientes = 0;
 
+    private static Camarero[] camareros = new Camarero[10];
+    private static int numCamareros = 0;
+
     public static void main(String[] args) {
 
         int opcion;
@@ -14,6 +17,7 @@ public class Main {
             System.out.println();
             System.out.println("==== CAFETERIA ====");
             System.out.println("1. Crear cliente");
+            System.out.println("2. Crear camarero");
             System.out.println("0. Salir \n");
 
             System.out.print("Seleccione una opción: ");
@@ -26,7 +30,7 @@ public class Main {
                 // =============================
                 case 1:
                     System.out.println();
-                    System.out.println("--- NUEVO CLIENTE  ---");
+                    System.out.println("--- NUEVO CLIENTE ---");
 
                     System.out.print("Nombre del nuevo cliente: ");
                     String nombreCliente = sc.nextLine();
@@ -43,6 +47,31 @@ public class Main {
                     // Descomentar bucle for para verificar la lista de clientes creados
                     //for (int i = 0; i < numClientes; i++){
                     //    System.out.print(clientes[i].toString());
+                    //}
+
+                    break;
+
+                // =============================
+                // CREAR CAMARERO
+                // =============================
+                case 2:
+                    System.out.println();
+                    System.out.println("--- NUEVO CAMARERO ---");
+
+                    System.out.print("Nombre del nuevo camarero: ");
+                    String nombreCamarero = sc.nextLine();
+
+                    System.out.print("Código de empleado: ");
+                    String codigoEmpleado = sc.nextLine();
+
+                    Camarero camarero = new Camarero(nombreCamarero, codigoEmpleado);
+                    camareros[numCamareros] = camarero;
+                    numCamareros++;
+                    System.out.println("Camarero Creado: " + camarero.toString());
+
+                    // Descomentar bucle for para verificar la lista de camareros creados
+                    //for (int i = 0; i < numCamareros; i++){
+                    //    System.out.print(camareros[i].toString());
                     //}
 
                     break;
