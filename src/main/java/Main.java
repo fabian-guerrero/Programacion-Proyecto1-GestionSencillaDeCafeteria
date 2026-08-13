ecc75973476106b2ad2c24ddf42d9fa3b39746c5
Fabian Guerrero <guerrerofabian1985@gmail.com>
2026-08-13 16:32:28 +0200

    Añadir funcionalidad para crear cliente

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,63 @@
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
+
+    private static Cliente[] clientes = new Cliente[10];
+    private static int numClientes = 0;
 
     public static void main(String[] args) {
 
+        int opcion;
+
+        do {
+            System.out.println();
+            System.out.println("==== CAFETERIA ====");
+            System.out.println("1. Crear cliente");
+            System.out.println("0. Salir \n");
+
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion){
+                // =============================
+                // CREAR CLIENTE
+                // =============================
+                case 1:
+                    System.out.println();
+                    System.out.println("--- NUEVO CLIENTE  ---");
+
+                    System.out.print("Nombre del nuevo cliente: ");
+                    String nombreCliente = sc.nextLine();
+
+                    System.out.print("Teléfono del nuevo cliente: ");
+                    String telefonoCliente = sc.nextLine();
+
+                    Cliente cliente = new Cliente(nombreCliente, telefonoCliente);
+                    clientes[numClientes] = cliente;
+                    numClientes++;
+                    System.out.println("Cliente Creado: " + cliente.toString());
+
+
+                    // Descomentar bucle for para verificar la lista de clientes creados
+                    //for (int i = 0; i < numClientes; i++){
+                    //    System.out.print(clientes[i].toString());
+                    //}
+
+                    break;
+
+                // =============================
+                // SALIR
+                // =============================
+                case 0:
+                    System.out.println("Cerrando aplicación...");
+                    break;
+
+                default:
+                    System.out.println("Opción no valida.");
+            }
+
+        }while (opcion != 0);
     }
 }
