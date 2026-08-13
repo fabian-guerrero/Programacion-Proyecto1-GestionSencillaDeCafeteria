f4fd5cb619aec9c7b585e2bfe61c1b4a79997c36
Fabian Guerrero <guerrerofabian1985@gmail.com>
2026-08-13 12:55:10 +0200

    Crear clase Producto

---
+++ src/main/java/Producto.java
@@ -0,0 +1,45 @@
+public class Producto {
+    protected String nombre;
+    protected double precio;
+    private String categoria;
+
+    public Producto(String nombre, double precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Producto{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", precio=").append(precio);
+        sb.append(", categoria='").append(categoria).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
